95045a3aac26f3495e9623c2f6a19fb7de1590c3
Bolaji Olumota <bolajiolumotaseun@gmail.com>
2022-06-22 16:40:24 +0100

    Second Commit batch 3

---
+++ app/Models/PackageLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PackageLog extends Model
+{
+    use HasFactory;
+
+    protected $table='package_log';
+
+    protected $fillable = [
+        'package_id',
+        'user_id'
+    ];
+
+}
